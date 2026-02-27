bfc52643afce5d2a0a641ef2c960993b3b3540d0
andrey-fl <andreystarch991@gmail.com>
2026-02-27 11:34:08 +0100

    lab1 zad 1

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main (String[] args) {
         System.out.println("Hello world");
+        Point p = new Point();
+        p.x= 5F;
+        p.y=7.5F;
+        System.out.println(p.toString());
+
     }
 
 }
