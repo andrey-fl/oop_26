549bd84e88f22781ef6e827f9eee4c3f504eca0e
andrey-fl <andreystarch991@gmail.com>
2026-02-27 12:00:31 +0100

    lab1 zad 2

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -5,7 +5,12 @@ public class Main {
         p.x= 5F;
         p.y=7.5F;
         System.out.println(p.toString());
-
+System.out.println(p.toSvg());
+        p.translate( -20, 5);
+        System.out.println(p);
+        Point p2=p.translated(-30F, -0.5F);
     }
+
+
 
 }
--- svg/src/Point.java
+++ svg/src/Point.java
@@ -1,6 +1,21 @@
+import java.util.Locale;
+
 public class Point {
     public float x,y;
 public String toString(){
     return "Point {x = " + x+ ", y = " + y +"}";
+}
+public String toSvg(){
+    return String.format(Locale.ENGLISH, "<circle r=\"20\" cx = \"%f\" cy = \"%f\" fill =\"red\" />", x, y);
+}
+public void translate(float dx, float dy){
+    x+=dx;
+    y+=dy;
+}
+public Point translated(float dx, float dy){
+    Point newpoint=new Point();
+    newpoint.x=this.x+dx;
+    newpoint.y=this.y +dy;
+    return newpoint;
 }
 }
